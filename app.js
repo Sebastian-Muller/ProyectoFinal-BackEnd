00fa8a3fc8adf7f7d866eb06d60c0558d5bc6763
Sebastian Müller <94447644+Sebastian-Muller@users.noreply.github.com>
2023-02-16 03:42:33 -0300

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-
+const pokemonsRouter = required('./routes/pokemons')
+      
 const app = express();
 
 //MIDDLEWARES
@@ -18,5 +19,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 //RUTAS
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use("/pokemons", pokemonsRouter)
 
 module.exports = app;
